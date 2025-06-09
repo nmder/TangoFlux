98a26c6b9d72395c253cf420c002fd32db908f4f
tot <n.majumder.2009@gmail.com>
2025-06-09 22:10:48 +0800

    Fix typo

--- finetune.sh
+++ finetune.sh
@@ -1,2 +1,2 @@
 
-CUDA_VISISBLE_DEVICES=0,1 accelerate launch --config_file='configs/accelerator_config.yaml' tangoflux/train.py   --checkpointing_steps="best" --save_every=5 --config='configs/tangoflux_config.yaml' --load_from_hf
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file='configs/accelerator_config.yaml' tangoflux/train.py   --checkpointing_steps="best" --save_every=5 --config='configs/tangoflux_config.yaml' --load_from_hf
